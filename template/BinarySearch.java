3978dcebc8d3f03b1875baf39dd35799293e8e0f
wanyang1 <296866643@qq.com>
2021-09-03 00:06:14 +0800

    ac

---
+++ template/BinarySearch.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[] { 0, 1, 1, 1, 1, 1, 1, 2, 3, 7, 7 };
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println(binarySearch.lowbound(0, a.length, 1, a));
+        System.out.println(binarySearch.upperbound(0, a.length, 1, a));
+    }
+
+    /**
+     * left=0,right=n
+     * python:bisect.bisect_left()
+     * 查询符合<value值的最小个数
+     * 
+     * @param left
+     * @param right
+     * @param value
+     * @param a
+     * @return
+     */
+    public int lowbound(int left, int right, int value, Integer[] a) {
+        // lowbound
+        while (left < right) {
+            int mid = (right + left) / 2;
+            if (a[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * left=0,right=n
+     * python:bisect.bisect_right()
+     * 查询符合>value值的个数
+     * @param left
+     * @param right
+     * @param value
+     * @param a
+     * @return
+     */
+    public int upperbound(int left, int right, int value, Integer[] a) {
+        // upperbound
+        while (left < right) {
+            int mid = (right + left) / 2;
+            if (a[mid] > value) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ template/Common.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Common {
+
+    /**
+     * a,b最大公约数
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    /**
+     * 快速幂求(a^count)%power
+     * 
+     * @param a
+     * @param count
+     * @param power
+     * @return
+     */
+    long quickpower(long a, long count, long power) {
+        long result = 1;
+        while (count != 0) {
+            if ((count & 1) == 1) {
+                result = (result * a) % power;
+            }
+            a = (a * a) % power;
+            count = count >> 1;
+        }
+        return result;
+    }
+
+    /**
+     * 求逆元
+     * @param a
+     * @param power
+     * @return
+     */
+    long inverse(long a, long power) {
+        return quickpower(a, power - 2, power);
+    }
+
+    public static void main(String[] args) {
+        Common common = new Common();
+
+        // 最大公约数
+        System.out.println("最大公约数:" + common.gcd(12, 8));
+
+        long mod = 998244353;
+        //求逆元
+        System.out.println("取逆元:" + 1 * common.quickpower(4, mod - 2, mod) % mod);
+
+        //组合数
+        int n = 11;
+        long[] fact = new long[n], infact = new long[n];
+        Arrays.fill(fact, 1);
+        Arrays.fill(infact, 1);
+        for (int i = 1; i < n; i++) {
+            fact[i] = fact[i - 1] * i % mod;
+            infact[i] = infact[i - 1] * common.quickpower(i, mod - 2, mod) % mod;
+        }
+        int x = 4, y = 2;
+        System.out.println("组合数C(" + x + "," + y + "):" + fact[x] * infact[y] % mod * infact[x - y] % mod);
+    }
+}
